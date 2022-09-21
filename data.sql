f919bffd8fa179df5a9f091af0179510ca9f17a2
Esmatullah Ahamdzai <ea.ahmadzai2020@gmail.com>
2022-09-21 12:32:30 +0430

    add data sql

--- data.sql
+++ data.sql
@@ -4,4 +4,10 @@ INSERT INTO animals(id,name,date_of_birth,escapet_attempts,neutured,weight_kg) v
 INSERT INTO animals(id,name,date_of_birth,escapet_attempts,neutured,weight_kg) values(2,'Gabumon', '2020-nov-15',2,true,8);
 INSERT INTO animals(id,name,date_of_birth,escapet_attempts,neutured,weight_kg) values(3,'Pikachu', '2021-jan-7',1,false,15.04);
 INSERT INTO animals(id,name,date_of_birth,escapet_attempts,neutured,weight_kg) values(4,'Devimon', '2017-may-12',5,true,11);
-
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (5, 'Charmander', '2020-FEB-8', 0, FALSE, 11);
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (6, 'Plantmon', '2021-nov-15', 2, true, 5.7);
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (7, 'Squirtle', '1993-april-2', 3, false, 12.13);
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (8, ' Angemon', '2005-jun-12', 1, true, 45);
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (9, 'Boarmon', '2005-jun-7', 7, true, 20);
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (10, 'Blossom', '1998-oct-13', 3, true, 17);
+INSERT INTO animals (id, name, date_of_birth, escapet_attempts, neutured, weight_kg) VALUES (11, 'Ditto', '2022-may-14', 4, true, 22);
